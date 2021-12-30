8acbada1e126e1e43aee7a5e8180b0a2652422f5
saifelleuch <sa2014if@gmail.com>
2021-12-29 16:44:48 -0800

    another one

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-from sys import argv
-sum = 0
-for i in argv[1:]:
-    sum = sum + int(i)
-print(sum)
+    from sys import argv
+    sum = 0
+    for i in argv[1:]:
+        sum = sum + int(i)
+    print(sum)
