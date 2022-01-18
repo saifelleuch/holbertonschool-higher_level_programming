d00150a16c92d16a90f6ff7f7b977c0240260471
saifelleuch <sa2014if@gmail.com>
2022-01-18 13:52:11 -0800

    another one

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""define class square"""
+
+
+class Square:
+    """ just a simple square """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
