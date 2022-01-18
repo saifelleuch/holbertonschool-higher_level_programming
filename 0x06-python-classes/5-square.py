5f6a1573d5963db8f012829684d9f30e1b8f729c
saifelleuch <sa2014if@gmail.com>
2022-01-18 14:49:58 -0800

    another one

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" just playing with class Square"""
+
+
+class Square:
+    """ define class Square """
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(0, self.__size):
+                print()
+                for j in range(0, self.__size):
+                    print("#", end="")
+        print("")
