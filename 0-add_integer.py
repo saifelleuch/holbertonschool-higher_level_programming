e70e9cddfdbd823ce7bf9b8dd6056f6a9db3c24d
saifelleuch <sa2014if@gmail.com>
2022-01-25 14:27:35 -0800

    another one

---
+++ 0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """ function to add two integer """
+    if type(a) is not int or float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int or float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    else:
+        return (a + b)
