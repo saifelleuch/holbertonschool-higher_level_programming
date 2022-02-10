5a3a22832410f45d26c2e8d01c834831d6558979
saifelleuch <sa2014if@gmail.com>
2022-02-10 11:29:55 -0800

    another one

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    elif a_dictionary = {}:
+    elif a_dictionary == { }:
         return None
     else:
         return max(a_dictionary, key=a_dictionary.get)
