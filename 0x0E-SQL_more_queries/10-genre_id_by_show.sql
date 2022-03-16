9414c8cda4ea1eaead3d4dcf89b32860ced97de4
saifelleuch <sa2014if@gmail.com>
2022-03-16 08:27:59 -0700

    another one

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres
 INNER JOIN tv_show ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
