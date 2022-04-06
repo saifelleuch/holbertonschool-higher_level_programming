7f9d3dc9056f99bfec08c4dc1a8c36a3fe8470e9
saifelleuch <sa2014if@gmail.com>
2022-04-06 13:49:30 +0100

    another one

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const selectString = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+let i = 0;
+while (i < selectString.length) {
+  console.log(selectString[i]);
+  i++;
+}
