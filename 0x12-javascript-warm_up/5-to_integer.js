cb73174a06e4ed2fff10b24602d29a23ced5a681
saifelleuch <sa2014if@gmail.com>
2022-04-06 04:50:55 +0100

    another one

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (typeof (process.argv[2]) !== 'number') {
+if (typeof (process.argv[2]) !== 'number' && isNaN(parseInt(process.argv[2]))) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(process.argv[2]));
