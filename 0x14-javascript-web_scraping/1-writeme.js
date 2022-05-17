71461bd8e6f84b2815de39c9a0f509f5cb2e5f42
saifelleuch <sa2014if@gmail.com>
2022-05-17 23:15:18 +0100

    another one

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const fs = require("fs");
+const fs = require('fs');
 const file = process.argv[2];
-fs.writeFile(file, 'utf-8', function(err, data) {
-    if (err) {
-        console.log("err");
-    } else {
-        console.log("err");
-    }
+fs.writeFile(file, 'utf-8', function (err, data) {
+  if (err) {
+    console.log('err');
+  } else {
+    console.log('err');
+  }
 });
