32be3db5c48d666c8d925c19e58dcd9a57ee82bc
saifelleuch <sa2014if@gmail.com>
2022-05-17 23:38:46 +0100

    another one

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const { request } = require('http');
+const url = process.argv[2];
+request(url, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code:' + response);
+  }
+});
