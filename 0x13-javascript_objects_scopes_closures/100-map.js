cc396441f4fdc1468915910d5a49a110de515058
saifelleuch <sa2014if@gmail.com>
2022-07-13 15:31:49 +0100

    another one

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-// imports an array and computes a new array
-
 const list = require('./100-data.js').list;
 console.log(list);
-
-let newArray = [];
-newArray = list.map((x, i) => x * i);
-console.log(newArray);
+const newList = list.map((x, index) => x * index);
+console.log(newList);
